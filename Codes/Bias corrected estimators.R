5f83ff861a0493446cfd31e7604d4699ab0b12f3
mich750 <michal.warmuz7@gmail.com>
2026-02-09 13:11:05 +0100

    making code more readable

--- Codes/Bias corrected estimators.R
+++ Codes/Bias corrected estimators.R
@@ -1,71 +1,50 @@
 sim=function(n){
-  bledy_bca=numeric(50)
-  bledy_bcm=numeric(50)
-  bledy_naiwne=numeric(50)
+  error_bca <- numeric(50)
+  error_bcm <- numeric(50)
+  error_naive <- numeric(50)
   for(i in 1:50){
-  gamma=runif(10, 1, 2)
-  alfa=runif(1, -0.5, 0.5)
-  p=c(0.25, 0.2, 0.15, 0.05, 0.05, 0.1, 0.02, 0.03, 0.05, 0.1)
-  pomylki=diag(10)*70+matrix(rpois(100, 3), nrow=10)
-  pomylki = sweep(pomylki, MARGIN = 2, STATS = colSums(pomylki), FUN = '/')
-  q=rbinom(n, 1, 0.7)
-  thety=sample(1:10, size=n, replace = T, prob = p)
-  y=rpois(n, exp(gamma[thety]+alfa*q))
-  d=sapply(1:n, function(x){sample(1:10, size = 1, prob = pomylki[, thety[x]])})
+  gamma <- runif(10, 1, 2)
+  alfa <- runif(1, -0.5, 0.5)
+  p <- c(0.25, 0.2, 0.15, 0.05, 0.05, 0.1, 0.02, 0.03, 0.05, 0.1)
+  misclass <- diag(10)*70+matrix(rpois(100, 3), nrow=10)
+  misclass = sweep(misclass, MARGIN = 2, STATS = colSums(misclass), FUN = '/')
+  q <- rbinom(n, 1, 0.7)
+  theta <- sample(1:10, size=n, replace = T, prob = p)
+  y <- rpois(n, exp(gamma[theta]+alfa*q))
+  d <- sapply(1:n, function(x){sample(1:10, size = 1, prob = misclass[, theta[x]])})
   
-  fpr=sum(sapply(2:10, function(x){sum(pomylki[x,]*p)-pomylki[x, x]*p[x]}))
-  omega_hat=(diag(sapply(2:10, function(x){sum(pomylki[x,]*p)}))-sweep(pomylki[2:10, 2:10], MARGIN = 2, STATS = p[2:10], FUN='*'))/fpr
-  omega_hat=as.matrix(Matrix::bdiag(matrix(0, 1, 1), omega_hat, matrix(0, 1, 1)))
+  fpr <- sum(sapply(2:10, function(x){sum(misclass[x,]*p)-misclass[x, x]*p[x]}))
+  omega_hat <- (diag(sapply(2:10, function(x){sum(misclass[x,]*p)}))-sweep(misclass[2:10, 2:10], MARGIN = 2, STATS = p[2:10], FUN='*'))/fpr
+  omega_hat = as.matrix(Matrix::bdiag(matrix(0, 1, 1), omega_hat, matrix(0, 1, 1)))
   
-  model_naiwny = glm(y ~ as.factor(d) + q, family = "poisson")
-  glm_wyniki=coef(model_naiwny)
-  psi_hat=glm_wyniki
-  d_factor=relevel(factor(d, levels=as.character(1:10)), ref='1')
-  X=model.matrix(~d_factor +q)
-  m=solve(t(X)%*%X/n)
+  glm_naive <- glm(y ~ as.factor(d) + q, family = "poisson")
+  glm_results <- coef(glm_naive)
+  psi_hat <- glm_results
+  d_factor <- relevel(factor(d, levels=as.character(1:10)), ref='1')
+  X <- model.matrix(~d_factor +q)
+  m <- solve(t(X)%*%X/n)
   
-  psi_bca=(diag(11)+fpr*m%*%omega_hat)%*%psi_hat
-  psi_bca[2:10]=psi_bca[2:10]+psi_bca[1]
-  bledy_bca[i]=sum((psi_bca-c(gamma, alfa))^2)
+  psi_bca <- (diag(11)+fpr*m%*%omega_hat)%*%psi_hat
+  psi_bca[2:10] <- psi_bca[2:10]+psi_bca[1]
+  error_bca[i] <- sum((psi_bca-c(gamma, alfa))^2)
   
-  psi_bcm=solve(diag(11)-fpr*m%*%omega_hat)%*%psi_hat
-  psi_bcm[2:10]=psi_bcm[2:10]+psi_bcm[1]
-  bledy_bcm[i]=sum((psi_bcm-c(gamma, alfa))^2)
+  psi_bcm <- solve(diag(11)-fpr*m%*%omega_hat)%*%psi_hat
+  psi_bcm[2:10] <- psi_bcm[2:10]+psi_bcm[1]
+  error_bcm[i] <- sum((psi_bcm-c(gamma, alfa))^2)
   
-  glm_wyniki[2:10]=glm_wyniki[2:10]+glm_wyniki[1]
-  bledy_naiwne[i]=sum((glm_wyniki-c(gamma, alfa))^2)
+  glm_results[2:10] <- glm_results[2:10]+glm_results[1]
+  error_naive[i] <- sum((glm_results-c(gamma, alfa))^2)
   }
-  return(list(bledy_bca, bledy_bcm, bledy_naiwne))
+  return(list(error_bca, error_bcm, error_naive))
 }
 
-wynik_1000=sim(1000)
-bledy_bca_1000=wynik_1000[[1]]
-bledy_bcm_1000=wynik_1000[[2]]
-bledy_naiwne_1000=wynik_1000[[3]]
-mean(bledy_bca_1000)
-mean(bledy_bcm_1000)
-mean(bledy_naiwne_1000)
+results_1000=sim(1000)
+results_10000=sim(10000)
+results_100000=sim(100000)
+results=as.data.frame(c(results_1000, results_10000, results_100000))
+colnames(results)=c('bca1000', 'bcm1000', 'naive1000', 'bca10000', 'bcm10000', 'naive10000', 'bca100000', 'bcm100000', 'naive100000')
+results=rbind(results, colMeans(results))
+rownames(results)[51]='Mean Error'
 
-wynik_10000=sim(10000)
-bledy_bca_10000=wynik_10000[[1]]
-bledy_bcm_10000=wynik_10000[[2]]
-bledy_naiwne_10000=wynik_10000[[3]]
-mean(bledy_bca_10000)
-mean(bledy_bcm_10000)
-mean(bledy_naiwne_10000)
-
-wynik_100000=sim(100000)
-bledy_bca_100000=wynik_100000[[1]]
-bledy_bcm_100000=wynik_100000[[2]]
-bledy_naiwne_100000=wynik_100000[[3]]
-mean(bledy_bca_100000)
-mean(bledy_bcm_100000)
-mean(bledy_naiwne_100000)
-
-wynik_bc=as.data.frame(c(wynik_1000, wynik_10000, wynik_100000))
-colnames(wynik_bc)=c('bca1000', 'bcm1000', 'naive1000', 'bca10000', 'bcm10000', 'naive10000', 'bca100000', 'bcm100000', 'naive100000')
-wynik_bc=rbind(wynik_bc, colMeans(wynik_bc))
-rownames(wynik_bc)[51]='Mean Error'
-save(wynik_bc, file = "Results/Bias corrected estimators results.RData")
-
+save(results, file = "Results/Bias corrected estimators results.RData")
 #stargazer::stargazer(wynik_bc, type='latex', title = "Results", label = "tab:summary", flip=T)
--- Codes/Combining probability and nonprobability samplings.R
+++ Codes/Combining probability and nonprobability samplings.R
@@ -1,111 +1,97 @@
+pred = function(coefs, sample){
+  return(coefs[1] + coefs[2]*sample[['x']] + coefs[3]*sample[['z']])
+}
+
 set.seed(42)
-N = 100000
-n_sim = 1000
-n_prob = 500
-n_nonprob = 1000
-n_audit = 1000
-nonprob_ratio = 0.7
-biases = matrix(0, n_sim, 8)
-colnames(biases) = c('gold', 'mass_imp', 'mass_impnaive', 'bca', 'bca_upgr', 'bcm', 'bcm_upgr', 'joint')
-
-coefs_naive = coefs_bca_upgraded = coefs_bca = matrix(0, n_sim, 3)
-colnames(coefs_naive) = colnames(coefs_bca) = colnames(coefs_bca_upgraded) = c('Intercept', 'x', 'z')
-
+N <- 100000
+n_sim <- 1000
+n_prob <- 500
+n_nonprob <- 1000
+n_audit <- 1000
+nonprob_ratio <- 0.7
+biases <- matrix(0, n_sim, 8)
+colnames(biases) <- c('gold', 'mass_imp', 'mass_impnaive', 'bca', 'bca_upgr', 'bcm', 'bcm_upgr', 'joint')
 
 for(i in 1:n_sim){
-  x = rnorm(N, 2, 1)
-  z = rbinom(N, 1, 0.7)
-  z_star = z
-  z_star[z == 1] = sample(c(0,1), size = NROW(z_star[z == 1]), prob = c(0.1, 0.9), replace = T)
-  z_star[z == 0] = sample(c(0,1), size = NROW(z_star[z == 0]), prob = c(0.93, 0.07), replace = T)
-  e = rnorm(N)
-  y = 1 + x - 2*z + e
-  y_mean = mean(y)
+  x <- rnorm(N, 2, 1)
+  z <- rbinom(N, 1, 0.7)
+  z_star <- z
+  #create measurement error
+  z_star[z == 1] <- sample(c(0,1), size = NROW(z_star[z == 1]), prob = c(0.1, 0.9), replace = T)
+  z_star[z == 0] <- sample(c(0,1), size = NROW(z_star[z == 0]), prob = c(0.93, 0.07), replace = T)
+  e <- rnorm(N)
+  y <- 1 + x - 2*z + e
+  y_mean <- mean(y)
   
-  pop_data = data.frame(x, z, z_star, y)
-  prob_sample = pop_data[sample(1:N, n_prob), ]
-  nonprob_sample_x_large = pop_data[as.numeric(sample(rownames(pop_data[x>2, ]), n_nonprob*nonprob_ratio)), ]
-  nonprob_sample_x_small = pop_data[as.numeric(sample(rownames(pop_data[x<=2, ]), n_nonprob*(1-nonprob_ratio))), ]
-  nonprob_sample = rbind(nonprob_sample_x_small, nonprob_sample_x_large)
+  pop_data <- data.frame(x, z, z_star, y)
+  prob_sample <- pop_data[sample(1:N, n_prob), ]
+  #create a biased non-probability sample
+  nonprob_sample_x_large <- pop_data[as.numeric(sample(rownames(pop_data[x>2, ]), n_nonprob*nonprob_ratio)), ]
+  nonprob_sample_x_small <- pop_data[as.numeric(sample(rownames(pop_data[x<=2, ]), n_nonprob*(1-nonprob_ratio))), ]
+  nonprob_sample <- rbind(nonprob_sample_x_small, nonprob_sample_x_large)
   
-  #gold standard estimator - uses precise values from probability sample, impossible in practice
-  gold_standard = lm(y ~ x + z, data = prob_sample)
-  gold_standard_coefs = gold_standard$coefficients
-  y_pred_gold_standard = gold_standard_coefs[1] + gold_standard_coefs[2] * prob_sample[['x']] + gold_standard_coefs[3] * prob_sample[['z']]
-  bias_gold_standard = mean(y_pred_gold_standard) - y_mean
-  biases[i, 1] = bias_gold_standard
+  #gold standard estimator - uses precise values from probability sample
+  #impossible in practice but used as a benchmark
+  gold_standard <- lm(y ~ x + z, data = prob_sample)
+  gold_standard_coefs <- gold_standard$coefficients
+  y_pred_gold_standard <- pred(gold_standard_coefs, prob_sample)
+  biases[i, 1] <- mean(y_pred_gold_standard) - y_mean
   
-  #mass imputation estimator - uses precise values from non-probability sample to estimate values of probability sample, impossible in practice
-  mass_imputation = lm(y ~ x + z,  data = nonprob_sample)
-  mass_imputation_coefs = mass_imputation$coefficients
-  y_pred_mass_imputation = mass_imputation_coefs[1]+mass_imputation_coefs[2]*prob_sample[['x']]+mass_imputation_coefs[3]*prob_sample[['z']]
-  bias_mass_imputation = mean(y_pred_mass_imputation) - y_mean
-  biases[i, 2] = bias_mass_imputation
+  #mass imputation estimator - uses precise values from non-probability sample to estimate values of probability sample
+  #impossible in practice but used as an benchmark
+  mass_imputation <- lm(y ~ x + z,  data = nonprob_sample)
+  mass_imputation_coefs <- mass_imputation$coefficients
+  y_pred_mass_imputation <- pred(mass_imputation_coefs, prob_sample)
+  biases[i, 2] <- mean(y_pred_mass_imputation) - y_mean
   
   #naive mass imputation estimator - uses data from non-probability sample with measurement errors but does not correct them
-  mass_imputation_naive = lm(y ~ x + z_star,  data = nonprob_sample)
-  mass_imputation_naive_coefs = mass_imputation_naive$coefficients
-  y_pred_mass_imputation_naive = mass_imputation_naive_coefs[1]+mass_imputation_naive_coefs[2]*prob_sample[['x']]+mass_imputation_naive_coefs[3]*prob_sample[['z']]
-  bias_mass_imputation_naive = mean(y_pred_mass_imputation_naive) - y_mean
-  biases[i, 3] = bias_mass_imputation_naive
-  coefs_naive[i, ] = mass_imputation_naive_coefs
+  mass_imputation_naive <- lm(y ~ x + z_star,  data = nonprob_sample)
+  mass_imputation_naive_coefs <- mass_imputation_naive$coefficients
+  y_pred_mass_imputation_naive <- pred(mass_imputation_naive_coefs, prob_sample)
+  biases[i, 3] <- mean(y_pred_mass_imputation_naive) - y_mean
   
   #estimate false positive rate on an audit sample
-  s_audit = pop_data[sample(1:nrow(pop_data), n_audit),]
-  fpr=sum(s_audit$z==0 & s_audit$z_star==1)/n_audit
-  errors=s_audit$z_star - s_audit$z
-  lambda=c(mean(errors), mean(s_audit$x*errors))
+  s_audit <- pop_data[sample(1:nrow(pop_data), n_audit),]
+  fpr <- sum(s_audit$z==0 & s_audit$z_star==1)/n_audit
+  errors <- s_audit$z_star - s_audit$z
+  lambda <- c(mean(errors), mean(s_audit$x*errors))
   
   #bca estimator - corrects measurement error from non-probability sample and then uses mass imputation
-  bca_classic = MLBC::ols_bca(y ~ z_star + x, data = nonprob_sample, fpr = fpr, m = n_audit)
-  bca_classic_coef = bca_classic$coef
-  y_pred_bca_classic = bca_classic_coef[3]+bca_classic_coef[2]*prob_sample[['x']]+bca_classic_coef[1]*prob_sample[['z']]
-  bias_bca_classic = mean(y_pred_bca_classic)-y_mean
-  biases[i, 4] = bias_bca_classic
-  coefs_bca[i, ] = rev(bca_classic_coef)
+  bca_classic <- MLBC::ols_bca(y ~ z_star + x, data = nonprob_sample, fpr = fpr, m = n_audit)
+  bca_classic_coef <- bca_classic$coef[c(3, 2, 1)]
+  y_pred_bca_classic <- pred(bca_classic_coef, prob_sample)
+  biases[i, 4] <- mean(y_pred_bca_classic)-y_mean
   
-  mass_imputation_coefs
-  bca_classic_coef
   #upgraded bca estimator - corrects measurement error from non-probability sample and then uses mass imputation 
-  ksi_hat=rbind(z_star, rep(1, N), x)
-  psi_hat=solve(1/N*ksi_hat%*%t(ksi_hat))%*%(1/N*ksi_hat%*%y)
-  
-  bca_upgraded=(diag(3)+solve(1/N*ksi_hat%*%t(ksi_hat))%*%(cbind(c(fpr, lambda), matrix(0, 3, 2))))%*%psi_hat
-  y_pred_bca_upgraded = bca_upgraded[2]+bca_upgraded[3]*prob_sample[['x']]+bca_upgraded[1]*prob_sample[['z']]
-  bias_bca_upgraded = mean(y_pred_bca_upgraded) - y_mean
-  biases[i, 5] = bias_bca_upgraded
-  coefs_bca_upgraded[i, ] = bca_upgraded[c(2, 3, 1)]
+  ksi_hat <- rbind(z_star, rep(1, N), x)
+  psi_hat <- solve(1/N*ksi_hat%*%t(ksi_hat))%*%(1/N*ksi_hat%*%y)
+  bca_upgraded <- (diag(3)+solve(1/N*ksi_hat%*%t(ksi_hat))%*%(cbind(c(fpr, lambda), matrix(0, 3, 2))))%*%psi_hat
+  bca_upgraded_coefs <- bca_upgraded[c(2, 3, 1)]
+  y_pred_bca_upgraded <- pred(bca_upgraded_coefs, prob_sample)
+  biases[i, 5] <- mean(y_pred_bca_upgraded) - y_mean
   
   #bcm estimator - corrects measurement error using asymptotical behavior of inverse matrices
-  bcm = MLBC::ols_bcm(y ~ z_star + x, data = nonprob_sample, fpr = fpr, m = n_audit)
-  bcm_coef = bcm$coef
-  y_pred_bcm = bcm_coef[3]+bcm_coef[2]*prob_sample[['x']]+bcm_coef[1]*prob_sample[['z']]
-  bias_bcm = mean(y_pred_bcm) - y_mean
-  biases[i, 6] = bias_bcm
+  bcm <- MLBC::ols_bcm(y ~ z_star + x, data = nonprob_sample, fpr = fpr, m = n_audit)
+  bcm_coef <- bcm$coef[c(3, 2, 1)]
+  y_pred_bcm <- pred(bcm_coef, prob_sample)
+  biases[i, 6] <- mean(y_pred_bcm) - y_mean
   
   #upgraded bcm estimator - corrects measurement error using asymptotical behavior of inverse matrices
-  bcm_upgr = solve(diag(3)-solve(1/N*ksi_hat%*%t(ksi_hat))%*%(cbind(c(fpr, lambda), matrix(0, 3, 2))))%*%psi_hat
-  y_pred_bcm_upgr = bcm_upgr[2] + bcm_upgr[3]*prob_sample[['x']]+bcm_upgr[1]*prob_sample[['z']]
-  bias_bcm_upgr = mean(y_pred_bcm_upgr) - y_mean
-  biases[i, 7] = bias_bcm_upgr
+  bcm_upgr <- solve(diag(3)-solve(1/N*ksi_hat%*%t(ksi_hat))%*%(cbind(c(fpr, lambda), matrix(0, 3, 2))))%*%psi_hat
+  bcm_upgr_coefs <- bcm_upgr[c(2, 3, 1)]
+  y_pred_bcm_upgr <- pred(bcm_upgr_coefs, prob_sample)
+  biases[i, 7] <- mean(y_pred_bcm_upgr) - y_mean
   
   #joint estimator - uses joint estimation to correct measurement error
-  joint = MLBC::one_step(y ~ z_star + x, data = nonprob_sample)
-  joint_coef = joint$coef
-  y_pred_joint = joint_coef[3] + joint_coef[2]*prob_sample[['x']]+joint_coef[1]*prob_sample[['z']]
-  biases[i, 8] = mean(y_pred_joint) - y_mean
+  joint <- MLBC::one_step(y ~ z_star + x, data = nonprob_sample)
+  joint_coef <- joint$coef[c(3, 2, 1)]
+  y_pred_joint <- pred(joint_coef, prob_sample)
+  biases[i, 8] <- mean(y_pred_joint) - y_mean
 }
-squared_errors = biases^2
-results_biases = colMeans(biases)
-results_mse = colMeans(squared_errors)
-results=as.data.frame(rbind(results_biases, results_mse))
-results
-
+squared_errors <- biases^2
+results_biases <- colMeans(biases)
+results_mse <- colMeans(squared_errors)
+results <- as.data.frame(rbind(results_biases, results_mse))
+save(results, file="Results/Combining probability and nonprobability sampling.RData")
 #stargazer::stargazer(rbind(results_biases, results_mse), type='latex', digits=4, title = "Results", label = "tab:summary", flip=T)
 
-# coef_true = matrix(rep(c(1, 1, -2), n_sim), nrow=n_sim, byrow = T)
-# View(coefs_bca - coef_true)
-# View(coefs_naive - coef_true)
-# View(coefs_bca-coefs_naive) 
-# View(coefs_bca)
-# View(coefs_naive)
